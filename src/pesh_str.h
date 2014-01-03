65ca0f28f9b97452428c1d37553bec6fed50517d
Jardel Weyrich <jweyrich@gmail.com>
2014-01-03 07:41:48 -0200

    Include acknowledgment notice for code based on PHP source.

--- src/pesh_str.h
+++ src/pesh_str.h
@@ -17,6 +17,11 @@
 
 	You should have received a copy of the GNU General Public License
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	--
+	
+	This product includes PHP software, freely available from 
+	<http://www.php.net/software/>.
 */
 
 #include <string.h>
